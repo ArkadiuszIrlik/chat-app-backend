fafe9be6a8ef393af303546e939968b1b3a29153
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-02-26 15:54:57 +0100

    Add authentication controller

---
+++ src/controllers/authController.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express';
+import argon2 from 'argon2';
+import User from '@models/User.js';
+
+export async function registerUser(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { email, password } = req.body;
+
+    const hashedPassword = await argon2.hash(password, {
+      type: argon2.argon2id,
+      memoryCost: 47104,
+      timeCost: 1,
+      parallelism: 1,
+      secret: Buffer.from(process.env.PASSWORD_PEPPER!),
+    });
+
+    await User.create({
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      message: 'User registered successfully',
+    });
+  } catch (err) {
+    return next(err);
+  }
+}
