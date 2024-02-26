340a487a678875799f091979c813c449ba868d06
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-02-26 20:36:16 +0100

    Refactor auth config
    Removed the default export to make importing the values look more
    consistent and clean.

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -3,6 +3,4 @@
 //   AUTH_JWT_MAX_AGE: 10 * 60 * 1000,
 // };
 export const REFRESH_TOKEN_MAX_AGE = 30 * 24 * 60 * 60 * 1000;
-const AUTH_JWT_MAX_AGE = 10 * 60 * 1000;
-
-export default AUTH_JWT_MAX_AGE;
+export const AUTH_JWT_MAX_AGE = 10 * 60 * 1000;
