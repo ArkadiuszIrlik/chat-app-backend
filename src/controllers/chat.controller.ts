4696e2e0753d66ff06242d6bb70168befa52392b
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-05-30 20:12:52 +0200

    Refactor getMessages chat controller function
    Added authorization logic so that users who aren't members of a server
    can't access the messages sent in the server.
    
    Also added a populate method for the "author" field and a check to test
    if the "chatId" param is valid.

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,14 +1,54 @@
+import { getEmailFromJwt } from '@helpers/auth.helpers.js';
 import ChatMessage from '@models/ChatMessage.js';
+import User from '@models/User.js';
 import { NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 export async function getMessages(
   req: Request,
   res: Response,
-  _next: NextFunction,
+  next: NextFunction,
 ) {
   const chatId = req.params.chatId;
+  if (!mongoose.Types.ObjectId.isValid(chatId)) {
+    const err = new Error('Invalid param');
+    err.status = 404;
+    return next(err);
+  }
+
+  const email = req.decodedAuth?.email || getEmailFromJwt(req.cookies.auth);
+  const user = await User.findOne({ email }).exec();
+
+  if (user === null) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
   const messages = await ChatMessage.find({ chatId })
     .sort({ postedAt: -1 })
-    .limit(20);
-  return res.json(messages);
+    .limit(20)
+    .populate('author', 'username profileImg')
+    .exec();
+
+  // No messages found
+  if (messages.length === 0) {
+    return res.status(404).json({ message: 'Messages not found' });
+  }
+
+  const isServerMessage = !!messages[0].get('serverId');
+  if (isServerMessage) {
+    const serverId = messages[0].serverId;
+    const isUserInServer = !!user?.serversIn.find((id) => id.equals(serverId));
+
+    if (!isUserInServer) {
+      return res
+        .status(403)
+        .json({ message: 'Unauthorized to access messages' });
+    }
+
+    return res
+      .status(200)
+      .json({ message: 'Messages returned', data: { messages } });
+  } else {
+    return res.status(404).json({ message: 'Invalid message format' });
+  }
 }
