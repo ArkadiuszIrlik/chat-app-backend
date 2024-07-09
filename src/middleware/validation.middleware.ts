eb7b3a2aefc84954408cd046696e2e630617a015
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-10 01:49:38 +0200

    Refactor validation middleware
    Refactored validation middleware to accept schema in a more simple and
    dev-friendly form.
    
    Also added improved typing and made the function persist transformed
    values onto the "request" object.

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,14 +1,33 @@
 import { NextFunction, Request, Response } from 'express';
 import { Schema } from 'yup';
+import * as Yup from 'yup';
 
-function validate(schema: Schema) {
+type ValidatedProperties = 'body' | 'params' | 'query';
+type ValidateObject = Partial<
+  Record<ValidatedProperties, Record<string, Schema>>
+>;
+
+function validate(validateObject: ValidateObject) {
   return async function (req: Request, res: Response, next: NextFunction) {
     try {
-      await schema.validate({
+      const processedValidateObject: Partial<
+        Record<ValidatedProperties, Schema>
+      > = {};
+      (Object.keys(validateObject) as ValidatedProperties[]).forEach(
+        (key) =>
+          (processedValidateObject[key] = Yup.object(validateObject[key])),
+      );
+
+      const resObject = (await Yup.object(processedValidateObject).validate({
         body: req.body,
         params: req.params,
         query: req.query,
+      })) as Record<ValidatedProperties, Record<string, any>>;
+
+      (Object.keys(resObject) as ValidatedProperties[]).forEach((property) => {
+        req[property] = { ...req[property], ...resObject[property] };
       });
+
       return next();
     } catch (err) {
       return res.status(404).json({ message: 'Invalid data format' });
