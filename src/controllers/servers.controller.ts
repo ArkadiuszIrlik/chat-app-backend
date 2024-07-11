8128bfd9e086a00b80e192e3a35b36b9822f09c6
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-11 16:30:59 +0200

    Add processInviteCode servers controller

--- src/controllers/servers.controller.ts
+++ src/controllers/servers.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import * as serversService from '@services/servers.service.js';
 import * as usersService from '@services/users.service.js';
 import * as imagesService from '@services/images.service.js';
+import * as socketService from '@services/socket.service.js';
 import fileUpload from 'express-fileupload';
 
 export async function getServer(req: Request, res: Response) {
@@ -68,5 +69,52 @@ export async function generateInviteLink(req: Request, res: Response) {
   return res
     .status(201)
     .json({ message: 'Invite generated successfully', data: { inviteUrl } });
+}
+
+export async function processInviteCode(req: Request, res: Response) {
+  const inviteCode = req.body.inviteCode;
+  const invite = await serversService.findInvite(inviteCode);
+  if (!invite) {
+    return res.status(404).json({ message: 'Invalid invite code' });
+  }
+
+  const serverId = invite.server;
+  const serverPromise = serversService.getServer(serverId.toString());
+  const userPromise = usersService.getUser(req.decodedAuth?.userId ?? '');
+  const server = await serverPromise;
+  const user = await userPromise;
+  if (!server || !user) {
+    return res.status(500).json({ message: 'Server error' });
+  }
+
+  const isAlreadyInServerObj = await serversService.checkIfUserIsMember(
+    server,
+    user._id.toString(),
+  );
+  const isAlreadyInUserObj = await usersService.checkIfIsInServer(
+    user,
+    server._id.toString(),
+  );
+
+  // in case the two became desynced by accident, only throw 404 when both objects
+  // already have each other listed
+  if (isAlreadyInServerObj && isAlreadyInUserObj) {
+    return res
+      .status(404)
+      .json({ message: 'You are already a member in this server' });
+  }
+
+  if (!isAlreadyInServerObj) {
+    await serversService.addMember(server, user._id.toString());
+  }
+  if (!isAlreadyInUserObj) {
+    await usersService.addServerAsMember(user, server._id);
+  }
+
+  socketService.emitUserJoinedServer(req.socketIo, user, server);
+
+  return res
+    .status(200)
+    .json({ message: 'Successfully joined server', data: { server } });
 }
 
