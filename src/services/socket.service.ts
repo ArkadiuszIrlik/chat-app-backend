6e5ff2d86b52ff13e3fd731185430a8b2f534e63
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-12 21:41:38 +0200

    Fix types in socket service

--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -1,11 +1,10 @@
-import { SocketEvents } from '@customTypes/socket.types.js';
+import { SocketEvents, SocketServer } from '@customTypes/socket.types.js';
 import { IServer } from '@models/Server.js';
 import { IUser } from '@models/User.js';
 import { HydratedDocument } from 'mongoose';
-import { Server } from 'socket.io';
 
 function emitUserJoinedServer(
-  socketIo: Server,
+  socketIo: SocketServer,
   user: HydratedDocument<IUser>,
   server: HydratedDocument<IServer>,
 ) {
@@ -16,7 +15,7 @@ function emitUserJoinedServer(
       username: user.username,
       profileImg: user.profileImg,
     },
-    server._id,
+    server._id.toString(),
   );
 }
 
