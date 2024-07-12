394147e4a03ca8d23c8c88503d1841a8e4e53934
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-12 17:15:38 +0200

    Add unit tests for users service

---
+++ tests/unit/services/users.test.ts
@@ -0,0 +1,78 @@
+import {
+  addServerAsMember,
+  checkIfIsInServer,
+  getUser,
+} from '@services/users.service.js';
+import mongoose from 'mongoose';
+
+jest.mock('@models/User.js', () => ({
+  findById: jest.fn(() => ({
+    exec: jest.fn(),
+  })),
+}));
+import User, { IUser } from '@models/User.js';
+import { getUserFixture } from '@tests/fixtures/data/dbDocs.js';
+import { HydratedDocument } from 'mongoose';
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('getUser', () => {
+  it('gets user from DB', async () => {
+    const mockUser = getUserFixture() as HydratedDocument<IUser>;
+    mockUser._id = new mongoose.Types.ObjectId();
+
+    (User.findById as jest.Mock).mockReturnValueOnce({
+      exec: jest.fn().mockResolvedValue(mockUser),
+    });
+    const returnedUser = await getUser(mockUser._id.toString());
+
+    expect(User.findById).toHaveBeenCalled();
+    expect(returnedUser?._id).toEqual(mockUser._id);
+  });
+});
+
+describe('addServerAsMember', () => {
+  it("adds provided serverId to user's serversIn array", async () => {
+    const mockUser = getUserFixture() as HydratedDocument<IUser>;
+    mockUser.save = jest.fn();
+    const serverId = new mongoose.Types.ObjectId();
+    const returnedUser = await addServerAsMember(mockUser, serverId);
+
+    const isServerInUserDoc = !!returnedUser.serversIn.find((id) =>
+      id.equals(serverId),
+    );
+    expect(isServerInUserDoc).toBe(true);
+  });
+});
+
+describe('checkIfIsInServer', () => {
+  it("returns true if serverId is in user's serversIn array", async () => {
+    const serverId = new mongoose.Types.ObjectId();
+    const mockUser = getUserFixture({
+      serversIn: [
+        new mongoose.Types.ObjectId(),
+        serverId,
+        new mongoose.Types.ObjectId(),
+      ],
+    }) as HydratedDocument<IUser>;
+
+    const returnedValue = await checkIfIsInServer(
+      mockUser,
+      serverId.toString(),
+    );
+    expect(returnedValue).toBe(true);
+  });
+
+  it("returns false if serverId is not in user's serversIn array", async () => {
+    const serverId = new mongoose.Types.ObjectId();
+    const mockUser = getUserFixture() as HydratedDocument<IUser>;
+
+    const returnedValue = await checkIfIsInServer(
+      mockUser,
+      serverId.toString(),
+    );
+    expect(returnedValue).toBe(false);
+  });
+});
