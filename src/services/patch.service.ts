70faa6f62e411f26a3d0ea60e9535e8d8150133a
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-13 15:40:49 +0200

    Add getPatchableServer patch service function

--- src/services/patch.service.ts
+++ src/services/patch.service.ts
@@ -1,3 +1,16 @@
+import { IServer } from '@models/Server.js';
+import { HydratedDocument } from 'mongoose';
+
+/** Takes a Server document and returns an object with a subset of
+ * its properties that are safe to use with JSON Patch.
+ */
+function getPatchableServer(server: HydratedDocument<IServer>) {
+  const { name, channelCategories } = server;
+  const serverImg = server.get('serverImg', null, { getters: false });
+  const patchableDoc = { name, serverImg, channelCategories };
+
+  return patchableDoc;
+}
 function updateCommandValue(patch: any[], pathToCommand: string, value: any) {
   patch.forEach((command) => {
     if (command?.path === pathToCommand) {
@@ -23,6 +36,7 @@ function checkIfPatchHasProperty(patch: any[], propertyPointer: string) {
 }
 
 export {
+  getPatchableServer,
   updateCommandValue,
   checkIfPatchHasProperty,
 };
