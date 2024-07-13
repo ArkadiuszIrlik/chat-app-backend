d35ac9cd9fe4b274ad983790262d84ef7923c58c
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-13 21:12:35 +0200

    Refactor getUserFromAuth users controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,14 +1,27 @@
 import jwt from 'jsonwebtoken';
 import User, { IUser } from '@models/User.js';
 import { Request, Response } from 'express';
+import * as usersService from '@services/users.service.js';
 
 export async function getUserFromAuth(req: Request, res: Response) {
-  const decoded = jwt.decode(req.cookies.auth)!;
-  const user: IUser = await User.findOne({ email: decoded.sub }).exec();
+  const accessingUserId = req.decodedAuth?.userId;
+  if (!accessingUserId) {
+    return res.status(401).json({ message: 'Missing user credentials' });
+  }
+  const user = await usersService.getUser(accessingUserId, {
+    populateServersIn: true,
+  });
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
 
   return res.json({
     _id: user._id,
     name: user.username,
     profileImg: user.profileImg,
+    serversIn: user.serversIn,
+    prefersOnlineStatus: user.prefersOnlineStatus,
   });
+}
+
 }
