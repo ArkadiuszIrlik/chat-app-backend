0e3d4f743120f7aba146a9d1113a710a2712a5d1
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-13 16:18:51 +0200

    Fix getServerFixture type error
    Added missing _id properties in nested subdocuments to reflect the new
    interface type declaration.

--- tests/fixtures/data/dbDocs.ts
+++ tests/fixtures/data/dbDocs.ts
@@ -23,13 +23,13 @@ function getServerFixture(dataOverride: Partial<IServer> = {}): IServer {
         name: 'Test Category 1',
         channels: [
           {
-            //   _id: new mongoose.Types.ObjectId(),
+            _id: new mongoose.Types.ObjectId(),
             name: 'Test Channel 1',
             type: 'text',
             socketId: new mongoose.Types.ObjectId(),
           },
           {
-            //   _id: new mongoose.Types.ObjectId(),
+            _id: new mongoose.Types.ObjectId(),
             name: 'Test Channel 2',
             type: 'text',
             socketId: new mongoose.Types.ObjectId(),
@@ -41,13 +41,13 @@ function getServerFixture(dataOverride: Partial<IServer> = {}): IServer {
         name: 'Test Category 2',
         channels: [
           {
-            //   _id: new mongoose.Types.ObjectId(),
+            _id: new mongoose.Types.ObjectId(),
             name: 'Test Channel 3',
             type: 'text',
             socketId: new mongoose.Types.ObjectId(),
           },
           {
-            //   _id: new mongoose.Types.ObjectId(),
+            _id: new mongoose.Types.ObjectId(),
             name: 'Test Channel 4',
             type: 'text',
             socketId: new mongoose.Types.ObjectId(),
