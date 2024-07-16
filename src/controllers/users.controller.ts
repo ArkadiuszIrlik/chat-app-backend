223d1deb4ee49eccbd6780d4b469de2a503f8db3
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-17 01:24:45 +0200

    Add updateUser users controller function

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,7 +1,11 @@
-import jwt from 'jsonwebtoken';
-import User, { IUser } from '@models/User.js';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import * as usersService from '@services/users.service.js';
+import * as patchService from '@services/patch.service.js';
+import * as uploadedFilesService from '@services/uploadedFiles.service.js';
+import * as imagesService from '@services/images.service.js';
+import * as socketService from '@services/socket.service.js';
+import fileUpload from 'express-fileupload';
+import { ImageObject } from '@src/typesModule.js';
 
 export async function getUserFromAuth(req: Request, res: Response) {
   const accessingUserId = req.decodedAuth?.userId;
@@ -14,14 +18,69 @@ export async function getUserFromAuth(req: Request, res: Response) {
   if (!user) {
     return res.status(404).json({ message: 'User not found' });
   }
-
-  return res.json({
-    _id: user._id,
-    name: user.username,
-    profileImg: user.profileImg,
-    serversIn: user.serversIn,
-    prefersOnlineStatus: user.prefersOnlineStatus,
-  });
 }
 
+export async function updateUser(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const userId = req.params.userId;
+
+  // type tested by validation middleware
+  const tempPatch = req.files?.patch as fileUpload.UploadedFile;
+
+  const patchBuffer = await uploadedFilesService.readTempFile(tempPatch);
+  uploadedFilesService.removeTempFile(tempPatch);
+
+  const patch = JSON.parse(patchBuffer.toString());
+  if (!Array.isArray(patch)) {
+    return res.status(404).json({ message: 'Invalid PATCH data' });
+  }
+
+  const isProfileImgUpdate = patchService.checkIfPatchHasProperty(
+    patch,
+    '/profileImg',
+  );
+
+  let profileImgObj: ImageObject | null = null;
+  if (isProfileImgUpdate) {
+    // type tested by validation middleware
+    const profileImgFile = req.files?.profileImg as fileUpload.UploadedFile;
+    if (!profileImgFile) {
+      return res.status(404).json({ message: 'Missing profileImg upload' });
+    }
+    profileImgObj = await imagesService.saveUserProfileImage(profileImgFile);
+    patchService.updateCommandValue(patch, '/profileImg', profileImgObj);
+  }
+
+  const user =
+    req.context.requestedUser ?? (await usersService.getUser(userId));
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  try {
+    await usersService.patchUser(user, patch);
+  } catch (err) {
+    if (isProfileImgUpdate && profileImgObj) {
+      await imagesService.removeImage(profileImgObj);
+    }
+    return next(err);
+  }
+
+  const userSocketRooms = await socketService.getRoomsUserIsIn(
+    req.socketIo,
+    userId,
+  );
+  socketService.emitUserUpdated(
+    req.socketIo,
+    userSocketRooms,
+    usersService.getClientSafeSubset(
+      user,
+      usersService.UserAuthLevel.OtherUser,
+    ),
+  );
+
+  return res.status(200).json({ message: 'User updated' });
 }
