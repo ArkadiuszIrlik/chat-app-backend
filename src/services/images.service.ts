984eeb074d5bdf835d5ca23c927d913f48712e27
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-16 16:03:33 +0200

    Add saveUserProfileImage images service function

--- src/services/images.service.ts
+++ src/services/images.service.ts
@@ -1,4 +1,4 @@
-import { SERVER_IMAGES_PATH } from '@config/data.config.js';
+import { SERVER_IMAGES_PATH, USER_IMAGES_PATH } from '@config/data.config.js';
 import { ImageObject } from '@src/typesModule.js';
 import fileUpload from 'express-fileupload';
 import * as filesService from '@services/files.service.js';
@@ -25,8 +25,12 @@ async function saveServerImage(imageBuffer: fileUpload.UploadedFile) {
   return _saveImageAsset(imageBuffer, SERVER_IMAGES_PATH);
 }
 
+async function saveUserProfileImage(imageBuffer: fileUpload.UploadedFile) {
+  return _saveImageAsset(imageBuffer, USER_IMAGES_PATH);
+}
+
 function removeImage(pathname: string) {
   return filesService.removeFile(`./assets/${pathname}`);
 }
 
-export { saveServerImage, removeImage };
+export { saveServerImage, saveUserProfileImage, removeImage };
