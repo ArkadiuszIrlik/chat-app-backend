52cc5214cd0493bace097ff7b177a152353aee49
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-19 21:16:00 +0200

    Add getTempUserVerificationToken service function

--- src/services/tempUsers.service.ts
+++ src/services/tempUsers.service.ts
@@ -1,5 +1,20 @@
 import { TEMP_USER_MAX_AGE } from '@config/auth.config.js';
-import TempUser from '@models/TempUser.js';
+import TempUser, { ITempUser } from '@models/TempUser.js';
+import { HydratedDocument } from 'mongoose';
+
+async function _getTempUserFromParam(
+  tempUserParam: HydratedDocument<ITempUser> | string,
+) {
+  if (typeof tempUserParam === 'string') {
+    const tempUser = await TempUser.findById(tempUserParam).exec();
+    if (tempUser === null) {
+      throw Error('TempUser not found');
+    }
+    return tempUser;
+  } else {
+    return tempUserParam;
+  }
+}
 
 function createTempUser(email: string, hashedPassword: string) {
   return TempUser.create({
@@ -8,4 +23,13 @@ function createTempUser(email: string, hashedPassword: string) {
     expDate: new Date(Date.now() + TEMP_USER_MAX_AGE),
   });
 }
-export { createTempUser };
+
+async function getTempUserVerificationToken(
+  tempUser: string | HydratedDocument<ITempUser>,
+) {
+  const userToCheck = await _getTempUserFromParam(tempUser);
+
+  return userToCheck.verificationToken;
+}
+
+export { createTempUser, getTempUserVerificationToken };
