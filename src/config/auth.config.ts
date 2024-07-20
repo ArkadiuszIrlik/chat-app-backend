b48530fb63213127a6e6975c67f63543c3156e0f
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-21 01:56:24 +0200

    Move CLIENT_RESET_PASSWORD_URL constant
    Refactored CLIENT_RESET_PASSWORD_URL constant into
    CLIENT_RESET_PASSWORD_PATH and moved it from auth.config to
    client.config.

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,13 +1,5 @@
-import { getClientUrl } from '@helpers/fetch.helpers.js';
-
 const REFRESH_TOKEN_MAX_AGE = 30 * 24 * 60 * 60 * 1000;
 const AUTH_JWT_MAX_AGE = 10 * 60 * 1000;
 const TEMP_USER_MAX_AGE = 24 * 60 * 60 * 1000;
-const CLIENT_RESET_PASSWORD_URL = getClientUrl('/reset-password');
 
-export {
-  REFRESH_TOKEN_MAX_AGE,
-  AUTH_JWT_MAX_AGE,
-  TEMP_USER_MAX_AGE,
-  CLIENT_RESET_PASSWORD_URL,
-};
+export { REFRESH_TOKEN_MAX_AGE, AUTH_JWT_MAX_AGE, TEMP_USER_MAX_AGE };
