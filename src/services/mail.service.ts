c9479ff153b84b18cd1dacaa95adc703f5bbe6f1
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-21 01:40:17 +0200

    Refactor sendVerificationMail mail service
    sendVerificationMail function had too many responsibilites, making
    decisions for which mail to send shouldn't be part of its concern. It
    has been broken down into two separate functions:
    sendVerifyEmailNewAccount and sendExistingEmailRegisterAttempt.
    Calling the right one will be the concern of a controller making use
    of the mail service.
    
    Also moved the mailTransporter.use call into the top scope of the
    module.

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,65 +1,54 @@
 import { SENDER_ADDRESS, mailTransporter } from '@config/mail.config.js';
-import Mail from 'nodemailer/lib/mailer/index.js';
 import hbs from 'nodemailer-express-handlebars';
 import { TemplateMailOptions } from 'src/typesModule.js';
 
-async function sendVerificationMail(
-  to: Mail.Options['to'],
-  isExistingMail: false,
-  templateContext: { verificationUrl: string },
-): Promise<any>;
-async function sendVerificationMail(
-  to: Mail.Options['to'],
-  isExistingMail: true,
-  templateContext: {
-    resetPasswordUrl: string;
-    ipAddress: string;
-    location: string;
-    browserInfo: string;
-  },
-): Promise<any>;
-async function sendVerificationMail(
-  to: Mail.Options['to'],
-  isExistingMail: boolean,
-  templateContext: object,
-) {
-  const verificationEmailNew: TemplateMailOptions = {
+mailTransporter.use(
+  'compile',
+  hbs({
+    viewEngine: {
+      extname: '.hbs',
+      partialsDir: './src/views/mail',
+      layoutsDir: './src/views/mail',
+      defaultLayout: 'index',
+    },
+    extName: '.hbs',
+    viewPath: './src/views/mail',
+  }),
+);
+
+function sendVerifyEmailNewAccount(email: string, verificationUrl: string) {
+  const mailData: TemplateMailOptions = {
     from: SENDER_ADDRESS,
-    to: undefined,
+    to: email,
     subject: 'Verify your Beluga email address',
     template: 'verification-email-new',
-    context: {},
+    context: { verificationUrl },
   };
-
-  const verificationEmailExisting: TemplateMailOptions = {
-    from: SENDER_ADDRESS,
-    to: undefined,
-    subject: 'Beluga account creation attempt',
-    template: 'verification-email-existing',
-    context: {},
-  };
-
-  const mailData = isExistingMail
-    ? verificationEmailExisting
-    : verificationEmailNew;
-  mailData.to = to;
-  mailData.context = { ...templateContext };
-
-  mailTransporter.use(
-    'compile',
-    hbs({
-      viewEngine: {
-        extname: '.hbs',
-        partialsDir: './src/views/mail',
-        layoutsDir: './src/views/mail',
-        defaultLayout: 'index',
-      },
-      extName: '.hbs',
-      viewPath: './src/views/mail',
-    }),
-  );
 
   return mailTransporter.sendMail(mailData);
 }
 
-export { sendVerificationMail };
+function sendExistingEmailRegisterAttempt(
+  email: string,
+  resetPasswordUrl: string,
+  userTrackingInfo: {
+    ipAddress: string;
+    location: string;
+    browserInfo: string;
+  },
+) {
+  const mailData: TemplateMailOptions = {
+    from: SENDER_ADDRESS,
+    to: email,
+    subject: 'Beluga account creation attempt',
+    template: 'verification-email-existing',
+    context: {
+      resetPasswordUrl,
+      ...userTrackingInfo,
+    },
+  };
+
+  return mailTransporter.sendMail(mailData);
+}
+
+export { sendVerifyEmailNewAccount, sendExistingEmailRegisterAttempt };
