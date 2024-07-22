0014a4773a706c8309f348f855c990e397e7dd64
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-22 16:56:00 +0200

    Change global Request object types

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,9 +1,11 @@
+import { SocketServer } from '@customTypes/socket.types.ts';
 import { IServer } from '@models/Server.ts';
 import { IUser } from '@models/User.ts';
 import { HydratedDocument } from 'mongoose';
 declare global {
   namespace Express {
     export interface Request {
+      socketIo: SocketServer;
       decodedAuth?: {
         userId: string;
         email: string;
