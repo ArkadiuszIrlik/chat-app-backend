1195fcff125df2f8302fbd7d554f4083015223fb
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-25 11:21:48 +0200

    Fix STATIC_ASSETS_BASE_URL incorrect value
    The constant was incorrectly pointing to the frontend address instead
    of the backend address. Also the use of the "path" module to construct
    the URL was creating an issue where the double slash was being replaced
    with a single slash and breaking the URL.

--- src/config/data.config.ts
+++ src/config/data.config.ts
@@ -1,9 +1,8 @@
-import path from 'path';
 const SERVER_IMAGES_PATH = 'images/server/';
 const USER_IMAGES_PATH = 'images/user/';
-const STATIC_ASSETS_BASE_URL = path.join(
-  process.env.FRONTEND_ADDRESS ?? '',
+const STATIC_ASSETS_BASE_URL = new URL(
   'static',
+  process.env.BACKEND_ADDRESS ?? '',
 );
 
 export { SERVER_IMAGES_PATH, USER_IMAGES_PATH, STATIC_ASSETS_BASE_URL };
