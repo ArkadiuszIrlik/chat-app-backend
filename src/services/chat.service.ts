cb1e52df27ab5ede08bb0cb2f0c7ac70124f2256
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-07-26 15:49:35 +0200

    Add getClientSafeSubset chat service function

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -1,5 +1,5 @@
 import ChatMessage, { IChatMessage } from '@models/ChatMessage.js';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 
 function getMessages(
   chatId: string,
@@ -26,4 +26,72 @@ function getMessageServerId(message: HydratedDocument<IChatMessage>) {
   return message.serverId;
 }
 
-export { getMessages, checkIfIsServerMessage, getMessageServerId };
+enum ChatMessageAuthLevel {
+  Authorized = 'AUTHORIZED',
+}
+
+interface ClientSafeIChatMessage {
+  postedAt: Date;
+  author: Types.ObjectId;
+  text: string;
+  chatId: Types.ObjectId;
+  serverId?: Types.ObjectId;
+}
+
+/** Returns plain object subset of the provided chatMessage document
+ * with resolved getters. The subset of properties returned is
+ * determined by the provided authLevel.
+ *
+ * @param chatMessage ChatMessage document to subset
+ * @param authLevel authorization level, determines which properties
+ * of the ChatMessage doc are considered safe to return
+ * @returns plain object with resolved getters
+ */
+const authorizedAuthProperties = [
+  'postedAt',
+  'author',
+  'text',
+  'chatId',
+  'serverId',
+] as const;
+
+function getClientSafeSubset(
+  chatMessage: HydratedDocument<IChatMessage>,
+  authLevel: ChatMessageAuthLevel.Authorized,
+): Pick<ClientSafeIChatMessage, (typeof authorizedAuthProperties)[number]>;
+function getClientSafeSubset(
+  chatMessage: HydratedDocument<IChatMessage>,
+  authLevel: ChatMessageAuthLevel,
+): ClientSafeIChatMessage {
+  let safeProperties: (keyof ClientSafeIChatMessage)[] = [];
+  switch (authLevel) {
+    case ChatMessageAuthLevel.Authorized:
+      {
+        safeProperties = [...authorizedAuthProperties];
+      }
+      break;
+    default: {
+      safeProperties = [...authorizedAuthProperties];
+    }
+  }
+
+  const plainObjectMessage = chatMessage.toObject({ getters: true });
+
+  // type assertion is necessary since mongoose disregards
+  // getter return types
+  const clientSubset = Object.fromEntries(
+    safeProperties
+      .filter((key) => key in plainObjectMessage)
+      .map((key) => [key, plainObjectMessage[key]]),
+  ) as unknown as ClientSafeIChatMessage;
+
+  return clientSubset;
+}
+
+export {
+  getMessages,
+  checkIfIsServerMessage,
+  getMessageServerId,
+  getClientSafeSubset,
+  ChatMessageAuthLevel,
+};
