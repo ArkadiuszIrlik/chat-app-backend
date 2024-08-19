c4ed74d40d4969c32cb531bdb32841b0f3be58c7
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-08-19 22:17:13 +0200

    Add onHandshake socket middleware
    This helper middleware allows conditionally adding middleware to
    socket requests only when they're a handshake request.

--- src/middleware/socket.middleware.ts
+++ src/middleware/socket.middleware.ts
@@ -1,5 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 
+function onHandshake(
+  callback: (req: Request, res: Response, next: NextFunction) => any,
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const isHandshake = req._query?.sid === undefined;
+    if (!isHandshake) {
+      return next();
+    }
+    return callback(req, res, next);
+  };
+}
+
 function initializeSocketRequest(
   req: Request,
   _res: Response,
@@ -9,4 +21,4 @@ function initializeSocketRequest(
   return next();
 }
 
-export { initializeSocketRequest };
+export { onHandshake, initializeSocketRequest };
