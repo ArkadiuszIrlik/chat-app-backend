65d584475c7938bfef53133df82664e5b48ef474
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-11-12 21:36:10 +0100

    Add validation middleware to getMessages endpoint

--- src/routes/chat.router.ts
+++ src/routes/chat.router.ts
@@ -1,8 +1,24 @@
 import express from 'express';
 import { getMessages } from '@controllers/chat.controller.js';
+import { validate } from '@middleware/validation.middleware.js';
+import { ExtendedYup } from '@src/extendedYup.js';
 
 const router = express.Router();
 
-router.get('/:chatId/messages', getMessages);
+router.get(
+  '/:chatId/messages',
+  validate({
+    params: {
+      chatId: ExtendedYup.string().mongooseId().required(),
+    },
+    query: {
+      cursor: ExtendedYup.date(),
+      limit: ExtendedYup.number()
+        .min(1, "limit can't be lower than 1")
+        .max(50, "limit can't be larger than 50"),
+    },
+  }),
+  getMessages,
+);
 
 export default router;
