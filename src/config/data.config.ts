41995ac0919cc559b08665d4cd63cf5c2947c2de
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-02 17:56:22 +0100

    Add USER_PRESET_IMAGES_PATH to data config

--- src/config/data.config.ts
+++ src/config/data.config.ts
@@ -1,7 +1,13 @@
 const SERVER_IMAGES_PATH = 'images/server/';
 const USER_IMAGES_PATH = 'images/user/';
+const USER_PRESET_IMAGES_PATH = 'images/user/preset-images/';
 const STATIC_ASSETS_BASE_URL = process.env.BACKEND_ADDRESS
   ? new URL('static', process.env.BACKEND_ADDRESS).toString()
   : '';
 
-export { SERVER_IMAGES_PATH, USER_IMAGES_PATH, STATIC_ASSETS_BASE_URL };
+export {
+  SERVER_IMAGES_PATH,
+  USER_IMAGES_PATH,
+  USER_PRESET_IMAGES_PATH,
+  STATIC_ASSETS_BASE_URL,
+};
