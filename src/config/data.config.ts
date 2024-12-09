9c941bc79247d43964a04835c0efca2ae64a558e
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-09 18:07:20 +0100

    Refactor data config

--- src/config/data.config.ts
+++ src/config/data.config.ts
@@ -1,7 +1,8 @@
 const SERVER_IMAGES_PATH = 'images/server/';
 const USER_IMAGES_PATH = 'images/user/';
-const STATIC_ASSETS_BASE_URL = process.env.BACKEND_ADDRESS
-  ? new URL('static', process.env.BACKEND_ADDRESS).toString()
-  : '';
+const STATIC_ASSETS_BASE_URL = new URL(
+  'static',
+  process.env.BACKEND_ADDRESS,
+).toString();
 
 export { SERVER_IMAGES_PATH, USER_IMAGES_PATH, STATIC_ASSETS_BASE_URL };
