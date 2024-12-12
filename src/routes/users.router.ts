a34ca0f274d20cc32edd2fc296d2aee9f70f16fb
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-12 16:15:13 +0100

    Add "users/:userId/account-status" POST endpoint

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,8 +1,32 @@
 import express from 'express';
-import { getUserFromAuth } from '@controllers/users.controller.js';
+import {
+  approveAccountStatus,
+  getUserFromAuth,
+} from '@controllers/users.controller.js';
+import { validate } from '@middleware/validation.middleware.js';
+import {
+  AuthRole,
+  restrictAccess,
+} from '@middleware/authorization.middleware.js';
+import { idParam, userSchema } from '@src/validationSchema.js';
 
 const router = express.Router();
 
 router.get('/self', getUserFromAuth);
+
+router.post(
+  '/:userId/account-status',
+  restrictAccess([AuthRole.Self]),
+  validate({
+    params: {
+      userId: idParam.required('Missing user id'),
+    },
+    body: {
+      username: userSchema.username.required('Missing username'),
+      profileImg: userSchema.profileImg.required('Missing profile image'),
+    },
+  }),
+  approveAccountStatus,
+);
 
 export default router;
