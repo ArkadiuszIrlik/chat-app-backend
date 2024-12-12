d3c630c111f77355f317bc3a9e07263b9032e029
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-12 17:35:00 +0100

    Add "presetProfileImgs" to images service

--- src/services/images.service.ts
+++ src/services/images.service.ts
@@ -3,6 +3,7 @@ import { SERVER_IMAGES_PATH, USER_IMAGES_PATH } from '@config/data.config.js';
 import { ImageObject } from '@src/typesModule.js';
 import fileUpload from 'express-fileupload';
 import * as filesService from '@services/files.service.js';
+import { getAssetUrl } from '@helpers/fetch.helpers.js';
 
 async function _saveImageAsset(
   imageBuffer: fileUpload.UploadedFile,
@@ -33,4 +34,42 @@ function removeImage(imageObject: ImageObject) {
   return filesService.removeFile(path.join('./assets/', imageObject));
 }
 
-export { saveServerImage, saveUserProfileImage, removeImage };
+const originalPresetProfileImgs: {
+  id: ImageObject;
+  altText: string;
+  pathname: string;
+}[] = [
+  {
+    altText: 'closeup futuristic soldier image',
+    id: 'images/user/preset-images/553df52533b6c45e59da0f5b1e2efc3d.png',
+    pathname: 'images/user/preset-images/553df52533b6c45e59da0f5b1e2efc3d.png',
+  },
+  {
+    altText: 'angry gorilla image',
+    id: 'images/user/preset-images/072e974791b34e994847f56f07bb92c0.png',
+    pathname: 'images/user/preset-images/072e974791b34e994847f56f07bb92c0.png',
+  },
+  {
+    altText: 'cartoon hedgehog image',
+    id: 'images/user/preset-images/fc240c93384ba3ff7685d552e0f8abb3.png',
+    pathname: 'images/user/preset-images/fc240c93384ba3ff7685d552e0f8abb3.png',
+  },
+  {
+    altText: 'cartoon barbarian image',
+    id: 'images/user/preset-images/27cf650f4eeb77dc01687f82453d1edc.png',
+    pathname: 'images/user/preset-images/27cf650f4eeb77dc01687f82453d1edc.png',
+  },
+];
+
+const presetProfileImgs = originalPresetProfileImgs.map((preset) => ({
+  id: preset.id,
+  altText: preset.altText,
+  url: getAssetUrl(preset.pathname),
+}));
+
+export {
+  presetProfileImgs,
+  saveServerImage,
+  saveUserProfileImage,
+  removeImage,
+};
