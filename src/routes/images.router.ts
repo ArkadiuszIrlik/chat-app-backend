16280f4cf059be0cffc6ceb93ab8e33c67d9c3fa
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-12 17:39:30 +0100

    Add "images/profile-img/presets" endpoint

--- src/routes/images.router.ts
+++ src/routes/images.router.ts
@@ -1,8 +1,12 @@
-import { uploadProfileImg } from '@controllers/images.controller.js';
+import {
+  getPresetProfileImages,
+  uploadProfileImg,
+} from '@controllers/images.controller.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/profile-img', uploadProfileImg);
+router.get('/profile-img/presets', getPresetProfileImages);
 
 export default router;
