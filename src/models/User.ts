b2a931f87c55ce3916f7949aa3430c18c45fb9af
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-13 17:37:34 +0100

    Refactor User model
    Re-added missing properties in schema declaration and renamed some
    of the existing properties to be more concise.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,54 @@
+import { getAssetUrl } from '@helpers/fetch.helpers.js';
 import mongoose, { Schema, Types } from 'mongoose';
+
+function getProfileImgUrl(imagePathname: string) {
+  if (!imagePathname) {
+    return '';
+  }
+
+  return getAssetUrl(imagePathname);
+}
 
 export interface IRefreshTokenObject {
   token: string;
   deviceId: string;
   expDate: Date;
 }
+
+export interface IUser {
   email: string;
   password: string;
   username: string;
   profileImg: string;
-  serversMember: Types.ObjectId[];
-  chatsMember: { userId: Types.ObjectId; chatId: Types.ObjectId }[];
+  serversIn: Types.ObjectId[];
+  chatsIn: { userId: Types.ObjectId; chatId: Types.ObjectId }[];
   friends: Types.ObjectId[];
   refreshTokens: IRefreshTokenObject[];
 }
 
 const UserSchema = new mongoose.Schema<IUser>({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  username: {
+    type: String,
+  },
+  // stores pathname to the file
+  profileImg: {
+    type: String,
+    _id: false,
+    get: getProfileImgUrl,
+  },
+  serversIn: [{ type: Schema.Types.ObjectId, ref: 'Server', default: [] }],
+  chatsIn: {
+    type: [
+      {
+        userId: { type: Schema.Types.ObjectId, ref: 'User' },
+        chatId: { type: Schema.Types.ObjectId, ref: 'Chat' },
+      },
+    ],
+    default: [],
+  },
+  friends: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
   refreshTokens: {
     type: [{ token: String, deviceId: String, expDate: Date }],
     default: [],
