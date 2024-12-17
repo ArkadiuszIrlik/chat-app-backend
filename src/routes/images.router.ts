bd66b54ba8507a0fc63ee9d6c6590bd1e318dd35
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-17 19:01:06 +0100

    Add "images/server-img" endpoints

--- src/routes/images.router.ts
+++ src/routes/images.router.ts
@@ -1,6 +1,8 @@
 import {
   getPresetProfileImages,
+  getPresetServerImages,
   uploadProfileImg,
+  uploadServerImg,
 } from '@controllers/images.controller.js';
 import express from 'express';
 
@@ -8,5 +10,7 @@ const router = express.Router();
 
 router.post('/profile-img', uploadProfileImg);
 router.get('/profile-img/presets', getPresetProfileImages);
+router.post('/server-img', uploadServerImg);
+router.get('/server-img/presets', getPresetServerImages);
 
 export default router;
