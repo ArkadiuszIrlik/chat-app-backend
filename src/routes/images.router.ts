9ad445a2ea7f35f65fc0532e0ed4059faca59b2f
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-17 19:41:49 +0100

    Add validation middleware to images router

--- src/routes/images.router.ts
+++ src/routes/images.router.ts
@@ -1,16 +1,52 @@
 import {
+  SUPPORTED_PROFILE_IMG_MIME_TYPES,
+  SUPPORTED_SERVER_IMG_MIME_TYPES,
+} from '@config/data.config.js';
+import {
   getPresetProfileImages,
   getPresetServerImages,
   uploadProfileImg,
   uploadServerImg,
 } from '@controllers/images.controller.js';
+import { validate } from '@middleware/validation.middleware.js';
+import { ExtendedYup } from '@src/extendedYup.js';
 import express from 'express';
 
 const router = express.Router();
 
-router.post('/profile-img', uploadProfileImg);
+router.post(
+  '/profile-img',
+  validate({
+    files: {
+      image: ExtendedYup.mixed()
+        .notArray('Single profile image expected')
+        .oneOfMimeType(
+          SUPPORTED_PROFILE_IMG_MIME_TYPES,
+          'Unsupported file format',
+        )
+        .notTruncated("Profile image can't be bigger than 2MB")
+        .required('Image upload missing'),
+    },
+  }),
+  uploadProfileImg,
+);
 router.get('/profile-img/presets', getPresetProfileImages);
-router.post('/server-img', uploadServerImg);
+router.post(
+  '/server-img',
+  validate({
+    files: {
+      image: ExtendedYup.mixed()
+        .notArray('Single server image expected')
+        .oneOfMimeType(
+          SUPPORTED_SERVER_IMG_MIME_TYPES,
+          'Unsupported file format',
+        )
+        .notTruncated("Server image can't be bigger than 2MB")
+        .required('Image upload missing'),
+    },
+  }),
+  uploadServerImg,
+);
 router.get('/server-img/presets', getPresetServerImages);
 
 export default router;
