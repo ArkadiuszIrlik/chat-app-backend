61ba9c2de31635022a384a31df2fbda638d8ea42
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2025-02-14 21:21:21 +0100

    Update mail config
    Changed mail provider and modified the sender address config.

--- src/config/mail.config.ts
+++ src/config/mail.config.ts
@@ -1,11 +1,11 @@
 import nodemailer from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer/index.js';
 
-const SENDER_ADDRESS: Mail.Options['from'] = `Beluga <${process.env.SMTP_USER}>`;
+const SENDER_ADDRESS: Mail.Options['from'] = `"Beluga" beluga@webdevguy.site`;
 
 const mailTransporter = nodemailer.createTransport({
   port: 587,
-  host: 'smtp-mail.outlook.com',
+  host: 'smtp-relay.brevo.com',
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
 });
 
