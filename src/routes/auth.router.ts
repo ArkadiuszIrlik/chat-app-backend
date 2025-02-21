277e68265b0594e74f57202a3528e58b97a32c14
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2025-02-21 14:41:08 +0100

    Set up input validation on /auth/signup route

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -4,11 +4,28 @@ import {
   registerUser,
   verifyEmail,
 } from '@controllers/auth.controller.js';
+import { validate } from '@middleware/validation.middleware.js';
+import { ExtendedYup } from '@src/extendedYup.js';
 import express from 'express';
 
 const router = express.Router();
 
-router.post('/signup', registerUser);
+router.post(
+  '/signup',
+  validate({
+    body: {
+      email: ExtendedYup.string()
+        .trim()
+        .email('Invalid email address.')
+        .required('Please enter an email.'),
+      password: ExtendedYup.string()
+        .min(8, 'Password has to be at least 8 characters long.')
+        .max(100, "Password can't be longer than 100 characters.")
+        .required('Please enter a password.'),
+    },
+  }),
+  registerUser,
+);
 router.post('/login', logInUser);
 router.get('/logout', logOutUser);
 router.get('/verify-email', verifyEmail);
