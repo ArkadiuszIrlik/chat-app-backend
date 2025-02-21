87e1fbac32bd5d0382d3d60b9cb5acec212af915
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2025-02-21 14:22:49 +0100

    Update ChatMessage schema
    Renamed "authorId" field to "author" to be less confusing when the
    field gets populated in mongoose. Introduced new data fields and
    corresponding type declarations.

--- src/models/ChatMessage.ts
+++ src/models/ChatMessage.ts
@@ -2,16 +2,20 @@ import mongoose, { Schema, Types } from 'mongoose';
 
 export interface IChatMessage {
   postedAt: Date;
-  authorId: Types.ObjectId;
+  author: Types.ObjectId;
   text: string;
   chatId: Types.ObjectId;
+  serverId?: Types.ObjectId;
+  clientId: string;
 }
 
 const ChatMessageSchema = new mongoose.Schema<IChatMessage>({
-  postedAt: Date,
-  authorId: { type: Schema.Types.ObjectId, ref: 'User' },
-  text: String,
-  chatId: Schema.Types.ObjectId,
+  postedAt: { type: Date, required: true, default: Date.now },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { type: String, required: true },
+  chatId: { type: Schema.Types.ObjectId, required: true },
+  serverId: { type: Schema.Types.ObjectId, ref: 'Server' },
+  clientId: String,
 });
 
 export default mongoose.model<IChatMessage>('ChatMessage', ChatMessageSchema);
