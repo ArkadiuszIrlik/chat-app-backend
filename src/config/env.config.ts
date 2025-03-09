9b2baa48ff356b127b04f7948efdbacac4f29071
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2025-03-09 12:49:56 +0100

    Revert "Fix env path in production"
    This reverts commit 52d206c2134a89bde15c9c5bd1fa10af4a5b6035.

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,17 +1,11 @@
 import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 import fs from 'fs';
-import path from 'path';
 
 if (!process.env.NODE_ENV) {
   throw Error('NODE_ENV environment variable missing');
 } else {
-  const envPath = path.resolve(
-    process.cwd(),
-    'env',
-    `.env.${process.env.NODE_ENV}`,
-  );
-
+  const envPath = `./env/.env.${process.env.NODE_ENV}`;
   try {
     fs.accessSync(envPath);
   } catch (err) {
