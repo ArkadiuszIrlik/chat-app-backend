6f41a1f307b0c5f5e16febbc7dc1ad82a0e05f29
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2025-03-12 11:55:03 +0100

    Turn setInfisicalToken into entrypoint script
    setInfisicalToken didn't work properly because the environment variable
    it set was limited to the scope of the script and wasn't available
    to Infisical tool. The script was reworked and renamed into general
    entrypoint script, which uses "exec" command to persist its environment
    variables and make them available inside the "npm run start" script.

--- ops/scripts/entrypoint.sh
+++ ops/scripts/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# Authenticates Infisical on container start
 export INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$(< /run/secrets/infisicalClientId) --client-secret=$(< /run/secrets/infisicalClientSecret)  --domain https://eu.infisical.com --plain --silent)
+exec "$@"
